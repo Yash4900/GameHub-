90452b9113bc338bda45e3afe1f9756e4653790e
Yash4900 <satra.y@somaiya.edu>
2020-05-02 00:03:42 +0530

    Background Music Added

--- gamehub.py
+++ gamehub.py
@@ -5,21 +5,33 @@ import threading
 import random
 import os
 import pygame
+import playsound
+pygame.init()
 
-def start_snakes(self):
+def start_snakes(*args):
+	pygame.mixer.music.stop()
+	playsound.playsound('sounds/SELECT.mp3')
 	root.withdraw()
 	os.system('python snakegame.py')
 	root.deiconify()
+	pygame.mixer.music.play()
 
-def start_flappy(self):
+def start_flappy(*args):
+	pygame.mixer.music.stop()
+	playsound.playsound('sounds/SELECT.mp3')
 	root.withdraw()
 
 	root.deiconify()
+	pygame.mixer.music.play()
 
-def start_racing(self):
+def start_racing(*args):
+	pygame.mixer.music.stop()
+	playsound.playsound('sounds/SELECT.mp3')
 	root.withdraw()
 	os.system('python cargame.py')
 	root.deiconify()
+	pygame.mixer.music.play()
+
 
 def pause():
 	progress['value'] = 20
@@ -35,6 +47,8 @@ def pause():
 	progress['value'] = 100
 	progress.pack_forget()
 	title.pack_forget()
+	load.pack_forget()
+	pygame.mixer.music.play()
 	select.pack(anchor=NW)
 	l4.pack(side=TOP, anchor=NW)
 	l1.pack(padx=45, side=LEFT)
@@ -52,13 +66,15 @@ root.geometry("800x500")
 root.maxsize(800,500)
 root.title("Gamehub")
 title = Label(root,text="Welcome To Gamehub",font="consolas 40", fg="white", bg="black")
-title.pack(pady=200)
+title.pack(pady=180)
+load = Label(root, text="Loading...", font="consolas", fg="white", bg="black")
+load.pack(side=TOP)
 progress = Progressbar(root,orient=HORIZONTAL,length=500,mode='determinate')
 progress.pack()
 t1 = threading.Thread(target=pause)
 t1.start()
-
-select = Label(root,text="Select a Game",font="consolas 30", fg="white", bg="black")
+pygame.mixer.music.load('sounds/Bgaudio.mp3')
+select = Label(root,text="Select your Game :)",font="consolas 30", fg="white", bg="black")
 snake = PhotoImage(file="img/snake.png")
 l1 = Label(root, image=snake, borderwidth=0)
 flappy = PhotoImage(file="img/flappy.png")
@@ -68,3 +84,4 @@ l3 = Label(root, image=carrace, borderwidth=0)
 line = PhotoImage(file="img/Line.png")
 l4 = Label(root, image=line, borderwidth=0)
 root.mainloop()
+
